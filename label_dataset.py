b5e09ab5940767e8965f0625a0ea66ef27088613
Marc Baetica <marc.baetica@gmail.com>
2019-03-04 00:39:52 -0500

    labeled images

--- label_dataset.py
+++ label_dataset.py
@@ -7,10 +7,24 @@
 import os
 import shutil
 
-#list the files in '.\\dataset'
-
-files = os.listdir('dataset')
-
-print(files)
+#list the files in 'dataset' folder
+files = os.listdir('dataset') #returns py list
+print("Files found: " + str(files))
 
 #move files to new folder '.\\labeled' with number + ''.png
+label=0
+for name in files:
+
+	baseurl=os.getcwd()
+	print('CWD:' + baseurl)
+
+	src=baseurl+'/dataset/'+name
+	dest=baseurl+'/labeled_dataset/'+str(label)+'.png'
+
+	print(src + ' ' + dest)
+	print("First file exists:" + str(os.path.isfile(src)))
+
+	print(name + " " + str(label))
+	shutil.copy(src, dest)
+	
+	label=label+1
