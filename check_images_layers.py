b8727248096ff745a35bc9d8ce72f6b790f76eab
Marc Baetica <marc.baetica@gmail.com>
2021-11-24 20:00:36 -0500

    Checking bands, graphing results, gitignore.

---
+++ check_images_layers.py
@@ -0,0 +1,26 @@
+import os
+from bokeh.plotting import figure, show
+from PIL import Image
+
+
+images = os.listdir('dataset')
+
+single = 0
+triple = 0
+quadruple = 0
+
+for image in images:
+    im = Image.open('dataset/' + image)
+    print(im.format, im.size, im.mode)
+    bands = im.mode
+    single = single+1 if bands == 'P' else single
+    triple = triple+1 if bands == 'RGB' else triple
+    quadruple = quadruple+1 if bands == 'RGBA' else quadruple
+
+x = [1, 2, 3]
+y = [single, triple, quadruple]
+
+
+p = figure()
+p.vbar(x=x, top=y, width=0.5)
+show(p)
